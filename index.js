74d1c32106bd97f1d82b48691290e0d5e80f468f
lctom <lctom.mendoza@prlbpo.technology>
2020-01-02 00:04:29 +0800

    testing

--- index.js
+++ index.js
@@ -4,56 +4,44 @@ const auth = require('./auth.json');
 const prefix = "t#";
 const colors = { spirit: "#b2f5ff", deft: "#fbe6a3", tough: "#ffadbe" }
 
+const fs = require("fs");
+client.msgs = require ("./msgs.json");
+
 client.on('ready', () => {
     console.log(`logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if(msg.content === prefix + 'profile'){
+client.on('message', message => {
+    if(message.content === prefix + 'profile'){
         let embed = new Discord.RichEmbed()
-            .setImage(msg.member.user.avatarURL)
-            .setTitle(msg.member.user.tag)
-            .setDescription(`${msg.member.user.username}'s Avatar`);
-        msg.channel.send(embed);
+            .setImage(message.member.user.avatarURL)
+            .setTitle(message.member.user.tag)
+            .setDescription(`${message.member.user.username}'s Avatar`);
+        message.channel.send(embed);
     }
-    if(msg.content === prefix + 'server'){
+    if(message.content === prefix + 'server'){
         let embed = new Discord.RichEmbed()
-            .setTitle(msg.guild.name)
+            .setTitle(message.guild.name)
             .setDescription("Server Information")
-            .setThumbnail(msg.guild.iconURL)
+            .setThumbnail(message.guild.iconURL)
             .setColor("#15f153")
-            .addField("Member Count", msg.guild.members.filter(member => !member.user.bot).size)
-            .addField("Bot Count", msg.guild.members.filter(member => member.user.bot).size);
-        msg.channel.send(embed);
+            .addField("Member Count", message.guild.members.filter(member => !member.user.bot).size)
+            .addField("Bot Count", message.guild.members.filter(member => member.user.bot).size);
+        message.channel.send(embed);
     }
-    if(msg.content === prefix + 'guides'){
-        let letter = { a: "🇦", b: "🇧", c: "🇨", d: "🇩"};
-        let embed = new Discord.RichEmbed()
-            .setThumbnail(msg.guild.iconURL)
-            .setColor(colors.spirit)
-            .addField("Guide Commands Master List", 
-                    `Listed here are the commands for managing guides.\nNavigate through these commands by reacting on this post.\n` + 
-                    `${letter.a} - Add Guides\n` +
-                    `${letter.b} - Show All Guides\n` +
-                    `${letter.c} - Remove Guides\n` +
-                    `${letter.d} - Manage your Guides`);
-        msg.channel.send(embed).then(botMessage => {
-            botMessage.react(letter.a)
-            .then(() => {
-                botMessage.react(letter.b).then(()=>{
-                    botMessage.react(letter.c).then(()=>{
-                        botMessage.react(letter.d);
-                    });
-                });
-            });
-            const filter = (reaction, user) => {
-                return Object.values(letter).includes(reaction.emoji.name) && user.id === msg.author.id;
-            }
-            botMessage.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] }).then(collected=>{
-                const reaction = collected.first();
-                console.log("[reaction]", reaction.emoji.name === letter.a);
-            })
+    if(message.content.startsWith("write")){
+        let editedMessage = message.content.slice(6);
+        client.messages[message.author.username] = {
+            message: message.content
+        }
+        fs.writeFile("./msgs.json", JSON.stringify(client.msgs, null, 4), err => {
+            if (err) throw err;
+            message.channel.send("message written");
         })
+    }
+    if(message.content.startsWith("get")){
+        let _message = client.msgs[message.author.username].message;
+        message.channel.send("message is: " + _message);
     }
 })
 
