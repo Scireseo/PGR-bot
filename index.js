969c02bef4714b33c19ba16c2edd609bb07ac50b
lctom <lctom.mendoza@prlbpo.technology>
2020-01-03 02:08:24 +0800

    -Finished 6* awareness -Handled some use-cases -Added daily missions

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-const titleCase = require('./helpers/characterCase.js');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
 const prefix = "~";
-const colors = { spirit: "#b2f5ff", deft: "#fbe6a3", tough: "#ffadbe" }
+// const colors = { spirit: "#b2f5ff", deft: "#fbe6a3", tough: "#ffadbe" }
+const colors = { weapons: "11728383", awareness: "16508579", character: "16756158" };
 
 const fs = require("fs");
+
+function capitalizeFirstLetter(string) {
+    if(string === "") return;
+    return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function titleCase(string) {
+    return string.split(" ").map(x => capitalizeFirstLetter(x)).join(" ");
+}
+
+function emoji(name) {
+    return client.emojis.find(emoji => emoji.name === name).toString();
+}
+
 client.msgs = require ("./msgs.json");
-client.awareness = require ("./awareness.json");
+client.awareness = require ("./json/awareness.json");
 
 client.on('ready', () => {
     console.log(`logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
+    if(msg.content.toLowerCase().includes("bully") || msg.content.toLowerCase().includes("bulli")){
+        if(!msg.author.bot) return msg.channel.send("no bully!");
+    }
     if(msg.content === prefix + 'profile'){
         let embed = new Discord.RichEmbed()
             .setImage(msg.member.user.avatarURL)
@@ -31,30 +48,60 @@ client.on('message', msg => {
             .addField("Bot Count", msg.guild.members.filter(member => member.user.bot).size);
         msg.channel.send(embed);
     }
-    if(msg.content.toLowerCase().startsWith(prefix + "awareness")){
-        let awarenessName = titleCase(msg.content.slice(11));
-        let selectedAwareness = client.awareness[awarenessName];
-        console.log("awareness: ", awarenessName, selectedAwareness);
-        if(selectedAwareness === undefined) return;
-        // let embed = new Discord.RichEmbed()
-        //     .setTitle(awarenessName)
-        //     .setThumbnail("attachment://hannah.png")
-        //     .setColor(colors.deft)
-        //     .addField("2 set effect", selectedAwareness.effect_1)
-        //     .addField("4 set effect", selectedAwareness.effect_2)
-        //     .addField("HP", selectedAwareness.base_HP + `(**${selectedAwareness.max_HP}**)`, true)
-        //     .addField("ATK", selectedAwareness.base_ATK + `(**${selectedAwareness.max_ATK}**)`, true)
-        //     .addField("DEF", selectedAwareness.base_DEF + `(**${selectedAwareness.max_DEF}**)`, true)
-        //     .addField("CRIT", selectedAwareness.base_CRIT + `(**${selectedAwareness.max_CRIT}**)`, true)
-        // msg.channel.send({embed, files: [{
-        //     attachment: './static/images/awareness/hannah.png',
-        //     name: 'hannah.png'
-        // }]});
+    if(msg.content.toLowerCase() === prefix + 'daily'){
         let embed = {
-            title: awarenessName,
-            colors: colors.deft,
+            title: "Daily missions",
+            fields: [{ 
+                name: "list of dailies", 
+                value: `
+                    • Login in 1 time
+                    • Clear any battle once
+                    • Enhance weapon
+                    • Enhance stigma
+                    • Enhance character
+                    • Purchase any item in the shop
+                    • Exchange black cards for stamina once
+                    • Consume 100 stamina
+                    • Consume 200 stamina
+                    • Consume 300 stamina
+                    • Talk to character in the home page once
+                    • Visit dorm once
+                    • Clear pain cage 3 times
+                    • Clear war zone 3 times
+                `
+            }]
+        }
+        msg.channel.send({ embed: embed });
+    }
+    if(msg.content.toLowerCase().startsWith(prefix + "awareness")){
+        let embed = {};
+        let awarenessName = titleCase(msg.content.replace(prefix + "awareness", "").trim());
+        let selectedAwareness = Object.keys(client.awareness["awareness"]).find(awareness => awareness.includes(awarenessName));
+        let awarenessIndex = Object.keys(client.awareness["awareness"]).findIndex(awareness => awareness.includes(awarenessName));
+        awarenessName = selectedAwareness;
+        selectedAwareness = client.awareness["awareness"][selectedAwareness];
+        if(awarenessName.toLowerCase() === "list"){
+            let listOfAwareness = Object.keys(client.awareness["awareness"]).map((awareness, index) => `${index + 1}.) ${awareness}`)
+            listOfAwareness = listOfAwareness.join("\r\n");
+            embed = {
+                title: "Awareness list",
+                fields: [{ name: "Names", value: listOfAwareness }]
+            }
+            return msg.channel.send({ embed: embed });
+        }
+        else if(selectedAwareness === undefined)
+            return msg.channel.send("The awareness that you're looking for does not exist. Be sure to check the list by typing the command `~awareness list`");
+        
+            let attachment = ("0" + awarenessIndex).slice(-2).concat(".png");
+        let rarity = "";
+        for(x = 0; x <= selectedAwareness.rarity - 1; x++){
+            rarity += "★";
+        }
+        embed = {
+            title: `${awarenessName}(${rarity})`,
+            color: colors.awareness,
             thumbnail: {
-                url: "attachment://hannah.png",
+                url: `attachment://${attachment}`,
             },
             fields: [
                 {
@@ -90,10 +137,24 @@ client.on('message', msg => {
         msg.channel.send({ 
             embed: embed,
             files: [{
-                attachment: './static/images/awareness/hannah.png',
-                name: 'hannah.png'
+                attachment: `./static/images/awareness/${attachment}`,
+                name: attachment
             }]
         });
+        
+        //move to a separate command
+        // let selectedLB = client.awareness["breakthrough"][selectedAwareness["rarity"]];
+        // console.log("rarity", selectedLB);
+        // let breakthrough = Object.keys(selectedLB).map((breakthrough, index) => 
+        //     `
+        //         ${index + 1}: ${selectedLB[breakthrough].nuts}x ${emoji("black_nuts")} 
+        //         ${selectedLB[breakthrough].awareness_low}x ${emoji("awareness_low")}
+        //         ${selectedLB[breakthrough].awareness_high}x ${emoji("awareness_high")}
+        //         ${selectedLB[breakthrough].alloy_low}x ${emoji("alloy_low")}
+        //         ${selectedLB[breakthrough].alloy_high}x ${emoji("alloy_high")}
+        //     `
+        // );
+        // breakthrough = breakthrough.join("\r\n");
     }
     // if(msg.content.startsWith("write")){
     //     let editedmsg = msg.content.slice(6);
