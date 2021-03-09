1cbcfa3e189973d04e1487d74811fa6a928f827c
Rexlent <rexlent@gmail.com>
2021-03-10 06:08:14 +0800

    removed tl credit, changed meme names and tooltips

--- commands/info.js
+++ commands/info.js
@@ -3,8 +3,7 @@ exports.run = (client, message, args) => {
     `Hi! I'm **${client.user.tag}**, created by **Kuu**!\nMy aim is to provide users info about the game **Punishing Gray Raven**!
     \n**Info**
     -**Author**: Kuu#6969
-    -**Translators**: Pinky#7220, The LoIi Hunter#6112, and ViceHampton#2514
-    -**Proofreader**: Rexlent#2020
+    -**Translators**: Rexlent#2020
     `;
     message.channel.send(msg);
 }
